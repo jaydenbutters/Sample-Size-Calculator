3aab75a7afe6d6cd859164b976cb97f0cc39ef24
Jayden Butterfill <work.jaydenbutterfill@gmail.com>
2025-11-25 19:52:03 +0800

    Fix import statement for plotly express

--- sample_size_calculator.py
+++ sample_size_calculator.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import math
 import pandas as pd
-import plotly.graph_express as px
+import plotly.express as px
 import plotly.graph_objects as go
 
 # Set page configuration
